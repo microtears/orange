3126658665d72b4d385c0eb035c8a1c0b0cd32a1
microtears <microtears@gmail.com>
2019-09-22 16:29:18 +0800

    - add ValueOr class

---
+++ livedata/src/main/java/com/microtears/orange/livedata/ValueOr.kt
@@ -0,0 +1,17 @@
+package com.microtears.orange.livedata
+
+data class ValueOr<T>(val value: T?, val throwable: Throwable?) {
+    companion object {
+        fun <E> fromThrowable(throwable: Throwable): ValueOr<E> {
+            return ValueOr<E>(null,  throwable);
+        }
+
+        fun <E> fromValue(value: E): ValueOr<E> {
+            return ValueOr<E>(value,null);
+        }
+    }
+
+    fun hasValue()=value!=null;
+    fun hasError()=throwable!=null;
+    
+}
