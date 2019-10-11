a15fc72827cebc8f21cd048e3f33a4dfd30227ba
microtears <microtears@gmail.com>
2019-10-11 21:08:07 +0800

    release livedata 1.1.0

---
+++ livedata/src/main/java/com/microtears/orange/livedata/Result.kt
@@ -0,0 +1,6 @@
+package com.microtears.orange.livedata
+
+sealed class Result<out T : Any> {
+    data class Success<out T : Any>(val data: T) : Result<T>()
+    data class Failure(val exception: Exception) : Result<Nothing>()
+}
